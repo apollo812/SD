4158d0246f9c47449b4c5aeb3bd9fea0b4a5e1bf
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-12 14:55:45 -0500

    fix: print

--- api/main.py
+++ api/main.py
@@ -6,7 +6,8 @@ current_script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Get the project root path
 project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir))
-
+print(current_script_directory)
+print(project_root)
 sys.path.append(project_root)
 
 from typing import List, Optional
